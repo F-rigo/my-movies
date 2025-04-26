c93524e39da3db23289a9acaef30318c9fece18e
f.rigo.dev@gmail.com <f.rigo.dev@gmail.com>
2025-04-26 21:47:55 +0100

    feat: update Movie type with additional fields

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,6 +1,16 @@
 export interface Movie {
   id: number
   title: string
+  overview: string
   poster_path: string
+  backdrop_path: string
+  release_date: string
   vote_average: number
+  videos?: {
+    results: Array<{
+      key: string
+      site: string
+      type: string
+    }>
+  }
 }
