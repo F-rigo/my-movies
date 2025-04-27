c5543a255c8558fd4847efc6f65639da8b22bdfb
f.rigo.dev@gmail.com <f.rigo.dev@gmail.com>
2025-04-27 13:03:10 +0100

    refactor: update GlobalStyle component to use theme

--- src/components/GlobalStyle/index.tsx
+++ src/components/GlobalStyle/index.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/global';
+import theme from '@/styles/theme';
+
 
 export default function GlobalStyleWrapper() {
-  return <GlobalStyle />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
